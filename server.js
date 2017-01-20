4155fc7f196275db2dffa3613e1403d104386a5d
jjmerchante <jj.merchante@gmail.com>
2017-01-20 12:05:54 +0100

    add https

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var https = require('https');
+var fs = require('fs');
+
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+options = {
+  key: fs.readFileSync('./conf/key.pem'),
+  cert: fs.readFileSync('./conf/cert.pem')
+}
+var server = https.createServer(options, app);
+server.listen(8000);
+var io = require('socket.io')(server);
+console.log('Running... https://localhost:8000')
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -11,20 +20,20 @@ app.use(express.static('public'));
 io.on('connection', function(socket) {
   var room;
 
-  socket.on('join', function(data){
-    room = data['room'];
-    var io_room = io.sockets.adapter.rooms[room];
-    if (io_room === undefined || io_room.length < 2){
-      console.log('joined', socket.id, 'to room', room );
-      socket.join(room);
-      io_room = io.sockets.adapter.rooms[room];
-      if (io_room.length == 2)
-        socket.broadcast.to(room).emit('requestOffer');
-    } else {
-      console.log('NOT joined', socket.id, 'to room', room, 'FULL' );
-      room = null;
-      io.to(socket.id).emit('error', 'full room');
-    }
+    socket.on('join', function(data){
+      room = data['room'];
+      var io_room = io.sockets.adapter.rooms[room];
+      if (io_room === undefined || io_room.length < 2){
+        console.log('joined', socket.id, 'to room', room );
+        socket.join(room);
+        io_room = io.sockets.adapter.rooms[room];
+        if (io_room.length == 2)
+          socket.broadcast.to(room).emit('requestOffer');
+      } else {
+        console.log('NOT joined', socket.id, 'to room', room, 'FULL' );
+        room = null;
+        io.to(socket.id).emit('error', 'full room');
+      }
     });
 
     socket.on('invite', function (sdp){
@@ -54,8 +63,4 @@ io.on('connection', function(socket) {
       socket.broadcast.to(room).emit('bye');
       console.log('disconnected', room, socket.id);
     });
-});
-
-http.listen(8000, function() {
-  console.log('listening on localhost:8000')
 });
